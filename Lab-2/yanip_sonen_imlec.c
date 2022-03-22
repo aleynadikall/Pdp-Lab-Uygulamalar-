b084224fe01ae882618f07a4b8a5ed9d345e0936
Aleyna Dikal <aleyynadikal@gmail.com>
2022-03-23 01:29:29 +0300

    \b karakteri

---
+++ Lab-2/yanip_sonen_imlec.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main() {
+	char c = '\b';
+	printf("Merhaba%c%c", c, c);
+	getchar();
+	return 0;
+}
+
+/*
+	Yukaridakikod blogunda c icerisinde bir karakter geri ifadesi tutuldugu icin ekrana Merhaba yazildiginda imleç b'nin üzerinde yanip sonecektir. 
+*/
