2ffcbfbd649f5532903ebaf0fb218e2c803d375b
Aleyna Dikal <aleyynadikal@gmail.com>
2022-03-23 00:49:50 +0300

    Java Arguments
    Java argüman projesi yapıldı.

---
+++ Lab-1/JavaArgumanProjesi/src/javaargumanprojesi/JavaArgumanProjesi.java
@@ -0,0 +1,34 @@
+//JAVA ARGUMAN PROJESI
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaargumanprojesi;
+
+/**
+ *
+ * @author aleyy
+ */
+public class JavaArgumanProjesi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(args[0]);        
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        
+        /*
+            Yukaridaki ifadelerin ciktisini gorebilmek icin menudeki Run sekmesine
+            ardindan Set Project Configuration diyip Customize'a tikliyoruz. Cikan
+            ekranda Arguments kismina diledigimiz cumleyi yaziyoruz. Cumledeki 
+            parametlerin (kelimeler) arasinda birer bosluk birakiyoruz. Her kelime
+            bir indekse denk gelecek sekilde ekrana yazdirilmis oluyor.
+        */
+
+    }
+    
+}
