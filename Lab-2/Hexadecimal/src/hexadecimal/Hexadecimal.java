b4c1c23d95daa91a823e27edb79dfea3757c02b2
Aleyna Dikal <aleyynadikal@gmail.com>
2022-03-23 01:37:45 +0300

    Hexadecimal
    A harfinin hexadecimal değeri ile ekrana yazdırılması

---
+++ Lab-2/Hexadecimal/src/hexadecimal/Hexadecimal.java
@@ -0,0 +1,28 @@
+/*
+    Java programlama dilinde karakteri ifade etmek icin farkli ozel bir yolu vardir.
+    Karakteri hexadecimal degerini yazdirarak ekrana cikartabilirsiniz.
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hexadecimal;
+
+/**
+ *
+ * @author aleyy
+ */
+public class Hexadecimal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char a = '\u0391';
+        System.out.println(a);
+        //Ekrana A harfini yazar.
+    }
+    
+}
