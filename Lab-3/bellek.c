f4feb12f1bebf805f1d9e14aae2a1e24fe36f6c3
Aleyna Dikal <aleyynadikal@gmail.com>
2022-03-26 21:45:49 +0300

    C ve Java Bellek Türleri

---
+++ Lab-3/bellek.c
@@ -0,0 +1,8 @@
+/*
+	Calisan herhangi bir programdaki degiskenler, siniflar, metotlar mutlaka bellekte bir yerde tutulmalidir.
+	Bellekte tutulduklari yerler bakimindan 3 farkli bolge bulunmaktadir:
+	1-Statik Bellek Bolgesi ( global degiskenler, sabitler, statik lokal degiskenler )
+	2-Calisma Ani Yiðini ( fonksiyonlar, metodlarin barindirdigi lokal degiskenler, donus degerleri, parametreler )
+	3-Heap Bellek Bolgesi ( Bu yapilara C'de pointer ile erisilebilir. C'de malloc, Java'da new )
+
+*/
