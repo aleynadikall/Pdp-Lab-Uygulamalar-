fb3c3f889c6aacb701273a7a2480f3408801cf55
Aleyna Dikal <aleyynadikal@gmail.com>
2022-03-26 23:35:39 +0300

    Java void Göstericisi

---
+++ Lab-3/void/src/pkgvoid/Void.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgvoid;
+
+/**
+ *
+ * @author aleyy
+ */
+public class Void {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Java'da C dilinde yaptiklarimiza gerek yoktur cunku Java'da object turu
+        // bulunmamaktadir. Sablon siniflar Java diline daha sonra eklenmistir.
+        
+        Object x = 100;
+        System.out.println(x);  //100
+        x = "Sakarya";
+        System.out.println(x);  //Sakarya
+        x = 28.12;
+        System.out.println(x);  //28.12
+
+
+    }
+    
+}
